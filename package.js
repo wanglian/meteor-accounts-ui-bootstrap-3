4738266d00799f0ff40169017d3caebef4b512cc
Janek Thomaschewski <janek@jbbr.net>
2015-09-19 17:43:33 +0200

    fix stylus dependency for meteor 1.2

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.on_use(function (api) {
 	api.use(['session@1.0.0',
 		'spacebars@1.0.0',
-		'stylus@1.0.0',
+		'stylus@1.0.0 || 2.0.0',
 		'accounts-base@1.0.0',
 		'underscore@1.0.0',
 		'templating@1.0.0',
